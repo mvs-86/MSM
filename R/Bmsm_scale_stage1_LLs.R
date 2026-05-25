c1a0c0d7d7cf91308801e9657d2ed89238c8e007
Marcus Santos <marcus@sigma2.info>
2026-05-25 19:06:30 -0300

    feat: add Bmsm_scale SE helper functions (LLs, hess)

---
+++ R/Bmsm_scale_stage1_LLs.R
@@ -0,0 +1,14 @@
+Bmsm_scale_stage1_LLs <- function(para, kbar, dat, n.vol) {
+  if (para[1] >= 2) para[1] <- 1.9999
+  if (para[2] >= 2) para[2] <- 1.9999
+  if (para[5] >= 1) para[5] <- 0.9999
+
+  ret1 <- matrix(dat[, 1], ncol = 1)
+  ret2 <- matrix(dat[, 2], ncol = 1)
+  par1 <- c(para[1], para[6], para[5], para[3], para[7])
+  par2 <- c(para[2], para[6], para[5], para[4], para[8])
+
+  LLs <- Amsm_scale_likelihood(par1, kbar, ret1, n.vol)$LLs +
+         Amsm_scale_likelihood(par2, kbar, ret2, n.vol)$LLs
+  return(LLs)
+}
---
+++ R/Bmsm_scale_stage2hess_LL.R
@@ -0,0 +1,23 @@
+Bmsm_scale_stage2hess_LL <- function(para, kbar, dat, n.vol, para1) {
+  if (para[1] >=  1) para[1] <-  0.9999
+  if (para[1] <= -1) para[1] <- -0.9999
+  if (para[2] >=  1) para[2] <-  0.9999
+  if (para[3] >=  1) para[3] <-  0.9999
+  if (para[3] <= -1) para[3] <- -0.9999
+
+  rhoe  <- para[1]; lamda <- para[2]; rho.m <- para[3]
+  m01     <- para1[1]; m02     <- para1[2]
+  sigma1  <- para1[3] / sqrt(n.vol); sigma2  <- para1[4] / sqrt(n.vol)
+  gamma.k <- para1[5]; b <- para1[6]; lev1 <- para1[7]; lev2 <- para1[8]
+
+  gm <- Bmsm_states(m01, m02, kbar)
+
+  if (kbar >= 4) {
+    LL <- Bmsm_scale_filtered_kron(dat, gm, rhoe, sigma1, sigma2,
+                                    b, gamma.k, lamda, rho.m, kbar, lev1, lev2)$LL
+  } else {
+    A  <- Bmsm_A(kbar, b, gamma.k, lamda, rho.m)
+    LL <- Bmsm_scale_filtered_cpp(dat, A, gm, rhoe, sigma1, sigma2, lev1, lev2)$LL
+  }
+  return(LL)
+}
